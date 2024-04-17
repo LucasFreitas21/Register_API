3d122c3c8896bba57d6910d41e8dcce4fe7d33e9
Lucas Freitas <freitas.lucas23@gmail.com>
2024-04-16 23:44:26 -0300

    stylization finished and Functionality started.

---
+++ assets/script/script.js
@@ -0,0 +1,13 @@
+const formulario = document.querySelector("form");
+
+const Inome = document.querySelector(".name");
+const Iidade = document.querySelector(".age");
+const Iemail = document.querySelector(".email");
+const Itel = document.querySelector(".tel");
+const Igravidez = document.querySelector(".gravidez");
+const Iparto = document.querySelector(".parto");
+const Ipass = document.querySelector(".pass");
+const IpassConf = document.querySelector(".pass-confirm");
+
+
+
